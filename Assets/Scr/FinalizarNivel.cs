d9b147374d6bbc7760b63bad3d5e68996f051b76
santiagosant <santidevargas@gmail.com>
2020-10-15 16:27:52 -0300

    implementado: menu de pausa, menu de nivel terminado generico, arreglos menores para evitar superposicion de menus

---
+++ Assets/Scr/FinalizarNivel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalizarNivel : MonoBehaviour
+{
+    public GameObject nivelTerminado;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        nivelTerminado.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scr/UiOpciones.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiOpciones : MonoBehaviour
+{
+
+    bool activado;
+    public GameObject menuDePausa;
+    public GameObject nivelTerminado;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !nivelTerminado.activeSelf)
+        {
+            activado = !activado;
+            menuDePausa.SetActive(activado);
+            Time.timeScale = (activado) ? 0 : 1f;
+        }
+    }
+
+}
